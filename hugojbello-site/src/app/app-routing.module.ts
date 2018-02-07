799c8bdc5dc8b1ef60ccab3fc7b255f5136dc001
Hugo José Bello Gutiérrez <hugo.bello.gu@gmail.com>
2018-02-07 18:27:55 +0100

    added auth0, route mapping

--- hugojbello-site/src/app/app-routing.module.ts
+++ hugojbello-site/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EntriesComponent } from './entries/entries.component';
+import { EntryComponent } from './entry/entry.component';
 
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth/auth.guard';
+import { CallbackComponent } from './callback.component';
 
 const routes:  Routes = [
   {
@@ -17,11 +21,27 @@ const routes:  Routes = [
   {
     path: 'entries',
     component: EntriesComponent
+  },
+  {
+    path: 'entry/:id',
+    component: EntryComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [
+      AuthGuard
+    ]
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule { }
--- hugojbello-site/src/app/app.module.ts
+++ hugojbello-site/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { CallbackComponent } from './callback.component';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { EntriesComponent } from './entries/entries.component';
+import { AdminComponent } from './admin/admin.component';
+import { EntryComponent } from './entry/entry.component';
 
 
 @NgModule({
@@ -14,13 +18,16 @@ import { EntriesComponent } from './entries/entries.component';
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    EntriesComponent
+    EntriesComponent,
+    AdminComponent,
+    CallbackComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
