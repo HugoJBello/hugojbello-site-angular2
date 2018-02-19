9e38bc2fe6aa2588745af636a0ec78694599d461
Hugo José Bello Gutiérrez <hugo.bello.gu@gmail.com>
2018-02-19 14:13:17 +0100

    added configs for spanish version

--- hugojbello-site/src/app/config/config.ts
+++ hugojbello-site/src/app/config/config.ts
@@ -12,12 +12,18 @@ interface Config {
 }
 var prod:boolean = true;
 var useLocalhost:boolean = false;
+var spanishVersion:boolean = true;
 
 var port:string;
+
 if (prod) {
   port = "";
-  var urlBase = "https://hjbello-site.firebaseapp.com"
-  //var url_base = 'https://hugojbello.github.io'
+  if(spanishVersion) {
+    var urlBase = "https://hjbello-blog-es.firebaseapp.com";
+  } else {
+    var urlBase = "https://hjbello-site.firebaseapp.com"; 
+  }
+  
 } else {
   port = "4200";
   var urlBase = "http://localhost";
@@ -26,17 +32,19 @@ if (prod) {
 if (useLocalhost) {
   var urlBackend="http://localhost:8080";
 } else {
-  var urlBackend = "https://hjbello-site-hjbello-site.7e14.starter-us-west-2.openshiftapps.com";
-  //var urlBackend = 'https://hjbello-site.herokuapp.com';
+  if (spanishVersion) {
+    var urlBackend = "https://hjbello-blog-es-hjbello-db.193b.starter-ca-central-1.openshiftapps.com";
+  } else {
+    var urlBackend = "https://hjbello-site-hjbello-site.7e14.starter-us-west-2.openshiftapps.com"; // 'https://hjbello-site.herokuapp.com'
+  }
 }
 
 export var CONFIG: Config = {
   CLIENT_ID: 'YTP9te890uUMscjE2sbA_IE51ztOMWNB',
   CLIENT_DOMAIN: 'cam-viewer-hjbello.eu.auth0.com', // e.g., you.auth0.com
-  //URL_BASE:'hjbello.hopto.org',
   URL_BASE: urlBase,
-  URL_BACKEND: urlBackend,//"http://localhost:8080",// 'https://hjbello-site.herokuapp.com',//
-  AUDIENCE: 'https://cam-viewer-hjbello.eu.auth0.com/userinfo', // e.g., http://localhost:3001
+  URL_BACKEND: urlBackend,
+  AUDIENCE: 'https://cam-viewer-hjbello.eu.auth0.com/userinfo', 
   REDIRECT: urlBase + ':' + port + '/callback',
   SCOPE: 'openid profile email',
   MAIN_PAGE_NAME:"main_page",
